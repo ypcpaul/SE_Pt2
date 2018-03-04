c68a39e67c2d00cad8071f00b3cf8d9d14552d8d
Paul Cayaco <ypaulc@gmail.com>
2018-03-04 15:38:35 +0800

    Fetch memory_check.sh file from github

--- test.pp
+++ test.pp
@@ -2,6 +2,7 @@ class test {
   package {'vim-enhanced':}
   package {'git':}
   package {'curl':}
+  package {'wget':}
 
   user { 'monitor':
     name => 'monitor',
@@ -15,6 +16,18 @@ class test {
     path => '/home/monitor/scripts',
     ensure => 'directory',
     require => User['monitor'],
+  }
+
+  exec { 'fetch_from_gh':
+    command => '/usr/bin/wget -q https://raw.githubusercontent.com/ypcpaul/SE_Pt1/master/memory_check.sh -O /home/monitor/scripts/memory_check.sh',
+    creates => '/home/monitor/scripts/memory_check.sh',
+    require => [ File['scriptsdir'], Package['wget'] ],
+  }
+
+  file { '/home/monitor/scripts/memory_check.sh':
+    ensure => 'file',
+    require => Exec['fetch_from_gh'],
+    mode => 0755,
   }
 }
 
