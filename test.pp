4825f9f50a40f3b07f944a5379c1c518dafc9528
Paul Cayaco <ypaulc@gmail.com>
2018-03-04 15:26:39 +0800

    Scripts directory

--- test.pp
+++ test.pp
@@ -3,12 +3,18 @@ class test {
   package {'git':}
   package {'curl':}
 
-  user {'monitor':
+  user { 'monitor':
     name => 'monitor',
     ensure => 'present',
     managehome => true,
     home => '/home/monitor',
     shell => '/bin/bash',
+  }
+
+  file { 'scriptsdir':
+    path => '/home/monitor/scripts',
+    ensure => 'directory',
+    require => User['monitor'],
   }
 }
 
